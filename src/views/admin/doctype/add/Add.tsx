a7af462a896917c619cd39ea13a1e2443c570c6d
Sohel Patel <spatel1@katalysttech.com>
2024-07-01 10:03:46 +0530

    Solved issue

--- src/views/admin/doctype/add/Add.tsx
+++ src/views/admin/doctype/add/Add.tsx
@@ -121,6 +121,7 @@ const Add: React.FunctionComponent<IAddProps> = () => {
   //Function to change current field
   const changeCurrentField = (currentIndex: number) => {
     setCurrentField(currentIndex);
+    console.log('fields[currentIndex] ', fields[currentIndex]);
     setFieldConfiguration(fields[currentIndex]);
   };
 
@@ -131,6 +132,7 @@ const Add: React.FunctionComponent<IAddProps> = () => {
     e: any
   ) => {
     console.log('fieldCode ', fieldCode)
+    console.log('e.target.value ', e.target.checked)
     setFieldConfiguration((prevField: any) => ({
       ...prevField,
       [fieldCode]: e.target.value,
@@ -144,6 +146,31 @@ const Add: React.FunctionComponent<IAddProps> = () => {
       return newFields;
     });
   };
+
+  //Function to handel confuguration change
+  const handelCheckboxChnage = (
+    parentSectionCode: any,
+    fieldCode: any,
+    e: any
+  ) => {
+    setFieldConfiguration((prevField: any) => ({
+      ...prevField,
+      [fieldCode]: e.target.checked,
+    }));
+
+    setFields((prevFields) => {
+      const newFields: any = [...prevFields];
+      if (newFields[currentField]) {
+        newFields[currentField][fieldCode] = e.target.checked;
+      }
+      return newFields;
+    });
+  };
+
+  //Function to handel doctypee submission
+  const handelDoctypeSubmit = () => {
+    console.log('Fields ', fields);
+  }
 
   return (
     <>
@@ -166,6 +193,11 @@ const Add: React.FunctionComponent<IAddProps> = () => {
               handelChange={() => console.log("hello")}
             />
           </div>
+          <div className="mt-8 flex justify-between">
+            <Button onClick={() => handelDoctypeSubmit()}>
+              Save
+            </Button>
+          </div>
         </Card>
         <Card heading="Configure Doctype Fields">
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4 mb-4">
@@ -184,7 +216,6 @@ const Add: React.FunctionComponent<IAddProps> = () => {
                     <option value="password">Password</option>
                     <option value="checkbox">Checkbox</option>
                     <option value="radio">Radio Button</option>
-                    <option value="button">Button</option>
                     <option value="file">Attach</option>
                     <option value="file_image">Attach Image</option>
                     <option value="date">Date</option>
@@ -295,31 +326,31 @@ const Add: React.FunctionComponent<IAddProps> = () => {
                   />
                 </div>
                 <div className="mt-2">
-                  <input type="checkbox" name="is_required" />
+                  <input type="checkbox" name="is_required" checked={fieldConfiguration?.isRequired} onChange={(e) => {handelCheckboxChnage("is_required", 'isRequired', e)}} />
                   <label className="text-sm font-bold text-[#102030] dark:text-gray-400 ml-2">
                     Is Mandatory
                   </label>
                 </div>
                 <div className="mt-2">
-                  <input type="checkbox" name="hidden" />
+                  <input type="checkbox" name="hidden" checked={fieldConfiguration?.hidden} onChange={(e) => {handelCheckboxChnage("hidden", 'hidden', e)}} />
                   <label className="text-sm font-bold text-[#102030] dark:text-gray-400 ml-2">
                     Hidden
                   </label>
                 </div>
                 <div className="mt-2">
-                  <input type="checkbox" name="read_only" />
+                  <input type="checkbox" name="read_only" checked={fieldConfiguration?.readOnly} onChange={(e) => {handelCheckboxChnage("read_only", 'readOnly', e)}} />
                   <label className="text-sm font-bold text-[#102030] dark:text-gray-400 ml-2">
                     Read Only
                   </label>
                 </div>
                 <div className="mt-2">
-                  <input type="checkbox" name="in_list_view" />
+                  <input type="checkbox" name="in_list_view" checked={fieldConfiguration?.inListView} onChange={(e) => {handelCheckboxChnage("in_list_view", 'inListView', e)}} />
                   <label className="text-sm font-bold text-[#102030] dark:text-gray-400 ml-2">
                     In List View
                   </label>
                 </div>
                 <div className="mt-2">
-                  <input type="checkbox" name="in_filter_list" />
+                  <input type="checkbox" name="in_filter_list" checked={fieldConfiguration?.inFilterList} onChange={(e) => {handelCheckboxChnage("in_filter_list", 'inFilterList', e)}} />
                   <label className="text-sm font-bold text-[#102030] dark:text-gray-400 ml-2">
                     In Filter List
                   </label>
